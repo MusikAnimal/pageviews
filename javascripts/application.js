c7fec29713ded71b21343e277f887d8a3815929f
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2016-02-11 14:18:51 +0100

    Add a polyfill for the String.includes function
    Support of IE and firefox<40, chrome<41 and safari<9

--- javascripts/application.js
+++ javascripts/application.js
@@ -9,6 +9,23 @@
 
 let normalized = false;
 
+// String.includes function polyfill
+// to add compatibility with older browsers
+if ( !String.prototype.includes ) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+    if (typeof start !== 'number') {
+      start = 0;
+    } 
+
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search,start) !== -1;
+    }
+  };
+}
+
 function setupProjectInput() {
   $(config.projectInput).on('change', function () {
     if(!this.value) {
