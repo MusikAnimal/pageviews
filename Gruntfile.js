a4c2a11cf0b21fa2daa5830e7ae2c339b35e450f
MusikAnimal <ziemba0123@gmail.com>
2016-02-16 20:46:11 -0500

    Can't run Travis locally, trying using specific version of eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
       }
     },
     eslint: {
-      src: ['javascripts/application.js']
+      src: ['javascripts/**/*.js']
     },
     scsslint: {
       options: {
