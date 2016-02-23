2629fa1ac1893cbbe5e62edf00d807e79104355c
MusikAnimal <ziemba0123@gmail.com>
2016-02-22 23:51:58 -0500

    topviews moving along, eslint and fixes cross-project

--- javascripts/topviews/application.js
+++ javascripts/topviews/application.js
@@ -1,26 +1,40 @@
 const config = require('./config');
 const siteMap = require('../shared/site_map');
+const siteDomains = Object.keys(siteMap).map(key => siteMap[key]);
 const pv = require('../shared/pv');
 let session = require('./session');
+window.session = session;
 
-function drawData(data, offset = 0) {
-  const max = data[0].views;
+function drawData(offset = 0) {
+  let count = offset, index = offset;
 
-  for (let index of [...Array(config.pageSize).keys()]) {
-    let item = session.pageData[index + offset];
+  while (count < config.pageSize) {
+    let item = session.pageData[index++ + offset];
+
+    if (session.excludes.includes(item.article.replace(/_/g, ' '))) continue;
+    if (!session.max) session.max = item.views;
+
+    let width = 100 * (item.views / session.max);
 
     // FIXME: loop only through first 100, then add "show more" link
-    // cache data
-    let width = 100 * (item.views / max);
-    $(".chart-container").append(
-      `<div class='topview-entry' style='background:linear-gradient(to right, #EEE ${width}%, transparent ${width}%)'>` +
-      `<span class='topview-entry--remove glyphicon glyphicon-remove' aria-hidden='true'></span>` +
-      `<span class='topview-entry--rank'>${index + offset + 1}</span>` +
-      `<a class='topview-entry--label' href="https://en.wikipedia.org/wiki/${item.article}" target="_blank">${item.article.replace(/_/g, ' ')}</a>` +
-      "<span class='topview-entry--leader'></span>" +
-      `<a class='topview-entry--views' href='/pageviews#pages=${item.article}'>${pv.n(item.views)}</a></div>`
+    $('.chart-container').append(
+      `<div class='topview-entry' style='background:linear-gradient(to right, #EEE ${width}%, transparent ${width}%)'>
+       <span class='topview-entry--remove glyphicon glyphicon-remove' data-article-id=${index - 1} aria-hidden='true'></span>
+       <span class='topview-entry--rank'>${++count}</span>
+       <a class='topview-entry--label' href="https://en.wikipedia.org/wiki/${item.article}" target="_blank">${item.article.replace(/_/g, ' ')}</a>
+       <span class='topview-entry--leader'></span>
+       <a class='topview-entry--views' href='${getPageviewsURL(item.article)}'>${pv.n(item.views)}</a></div>`
     );
   }
+
+  pushParams();
+
+  $('.topview-entry--remove').off('click').on('click', function() {
+    session.excludes.push(
+      session.pageNames[$(this).data('article-id')]
+    );
+    setArticleSelectorDefaults(session.excludes);
+  });
 }
 
 /**
@@ -33,6 +47,15 @@ function getDateFormat() {
   } else {
     return config.defaults.dateFormat;
   }
+}
+
+function getPageviewsURL(article) {
+  const startDate = $(config.dateRangeSelector).data('daterangepicker').startDate.format('YYYY-MM-DD'),
+    endDate = $(config.dateRangeSelector).data('daterangepicker').endDate.format('YYYY-MM-DD'),
+    platform = $('#platform-select').val(),
+    project = $(config.projectInput).val();
+
+  return `/pageviews#start=${startDate}&end=${endDate}&project=${project}&platform=${platform}&pages=${article}`;
 }
 
 /*
@@ -75,42 +98,52 @@ function popParams() {
   $(config.dateRangeSelector).data('daterangepicker').setEndDate(endDate);
   $('#platform-select').val(params.platform || 'all-access');
 
-  if (startDate < moment("2015-10-01") || endDate < moment("2015-10-01")) {
-    pv.addSiteNotice('danger', "Pageviews API does not contain data older than October 2015. Sorry.", "Invalid parameters!", true);
+  if (startDate < moment('2015-10-01') || endDate < moment('2015-10-01')) {
+    pv.addSiteNotice('danger', 'Pageviews API does not contain data older than October 2015. Sorry.', 'Invalid parameters!', true);
     resetView();
     return;
   } else if (startDate > endDate) {
-    pv.addSiteNotice('warning', "Start date must be older than the end date.", "Invalid parameters!", true);
+    pv.addSiteNotice('warning', 'Start date must be older than the end date.', 'Invalid parameters!', true);
     resetView();
     return;
   }
 
-  resetArticleSelector();
+  if (!params.excludes || (params.excludes.length === 1 && !params.excludes[0])) {
+    session.excludes = config.defaults.excludes;
+  } else {
+    session.excludes = params.excludes;
+  }
 
-  // if (!params.excludes || params.excludes.length === 1 && !params.excludes[0]) {
-  //   params.excludes = config.defaults.excludedPages;
-  //   setArticleSelectorDefaults(params.excludes);
-  // } else if (normalized) {
-  //   params.pages = pv.underscorePageNames(params.pages);
-  //   setArticleSelectorDefaults(params.pages);
-  // } else {
-  //   pv.normalizePageNames(params.pages).then((data)=> {
-  //     normalized = true;
+  initData().then(()=> {
+    setupArticleSelector(session.excludes);
+    drawData();
+  });
+}
 
-  //     params.pages = data;
+/**
+ * Replaces history state with new URL hash representing current user input
+ * Called whenever we go to update the chart
+ * @returns {string} the new hash param string
+ */
+function pushParams() {
+  const daterangepicker = $(config.dateRangeSelector).data('daterangepicker');
 
-  //     if (params.pages.length === 1) {
-  //       session.chartType = localStorage['pageviews-chart-preference'] || 'Bar';
-  //     }
+  const state = $.param({
+    start: daterangepicker.startDate.format('YYYY-MM-DD'),
+    end: daterangepicker.endDate.format('YYYY-MM-DD'),
+    project: $(config.projectInput).val(),
+    platform: $('#platform-select').val()
+  }) + `&excludes=${pv.underscorePageNames(session.excludes).join('|')}`;
 
-  //     setArticleSelectorDefaults(pv.underscorePageNames(params.pages));
-  //   });
-  // }
+  if (window.history && window.history.replaceState) {
+    window.history.replaceState({}, 'Topviews comparsion', '#' + state);
+  }
+
+  return state;
 }
 
 /**
  * Removes all article selector related stuff then adds it back
- * Also calls updateChart
  * @returns {null} nothing
  */
 function resetArticleSelector() {
@@ -119,9 +152,7 @@ function resetArticleSelector() {
   articleSelector.select2('val', null);
   articleSelector.select2('data', null);
   articleSelector.select2('destroy');
-  $('.data-links').hide();
   setupArticleSelector();
-  updateChart();
 }
 
 /**
@@ -129,28 +160,37 @@ function resetArticleSelector() {
  * @returns {null} nothing
  */
 function resetView() {
-  $(".chart-container").html("");
-  $(".chart-container").removeClass("loading");
-  $(".message-container").html("");
+  $('.chart-container').html('');
+  $('.chart-container').removeClass('loading');
+  $('.message-container').html('');
   resetArticleSelector();
 }
 
 /**
  * Sets up the article selector and adds listener to update chart
+ * @param {array} excludes - default page names to exclude
  * @returns {null} - nothing
  */
-function setupArticleSelector() {
+function setupArticleSelector(excludes) {
   const articleSelector = $(config.articleSelector);
 
   articleSelector.select2({
     data: session.pageNames,
-    defaults: config.defaults.excludedPages,
     maximumSelectionLength: 50,
     minimumInputLength: 0,
     placeholder: 'Type page names to exclude from view...'
   });
 
-  // articleSelector.on('change', updateChart);
+  if (excludes) setArticleSelectorDefaults(excludes);
+
+  articleSelector.on('change', function() {
+    session.excludes = $(this).val() || [];
+    session.max = null;
+    session.offset = 0;
+    $('.chart-container').html('');
+    drawData();
+    $(this).select2().trigger('close');
+  });
 }
 
 /**
@@ -160,7 +200,7 @@ function setupArticleSelector() {
  * @returns {array} - untouched array of pages
  */
 function setArticleSelectorDefaults(pages) {
-  $(config.articleSelector).val(pages).trigger('change');
+  $(config.articleSelector).select2().val(pages).trigger('change');
   return pages;
 }
 
@@ -179,12 +219,12 @@ function setupDateRangeSelector() {
 
   /** so people know why they can't query data older than October 2015 */
   $('.daterangepicker').append(
-    "<div class='daterange-notice'>" +
-    "Pageviews Analysis provides data from October 2015 forward. For older data, try <a href='http://stats.grok.se' target='_blank'>stats.grok.se</a>." +
-    "</div>"
+    `<div class='daterange-notice'>
+     Pageviews Analysis provides data from October 2015 forward. For older data, try <a href='http://stats.grok.se' target='_blank'>stats.grok.se</a>.
+     </div>`
   );
 
-  // dateRangeSelector.on('change', updateChart);
+  // dateRangeSelector.on('change', initData);
 }
 
 /**
@@ -203,44 +243,22 @@ function setupProjectInput() {
 }
 
 /**
- * Checks value of project input and validates it against site map
- * @returns {boolean} whether the currently input project is valid
+ * @returns {Deferred} promise with data fetched from API
  */
-function validateProject() {
-  const project = $(config.projectInput).val();
-  if (siteDomains.includes(project)) {
-    $(".validate").remove();
-    $(".select2-selection--multiple").removeClass('disabled');
-  } else {
-    resetView();
-    writeMessage(
-      `<a href='//${project}'>${project}</a> is not a ` +
-      "<a href='//meta.wikipedia.org/w/api.php?action=sitematrix&formatversion=2'>valid project</a>",
-      'validate', true
-    );
-    $(".select2-selection--multiple").addClass('disabled');
-    return true;
-  }
-}
+function initData() {
+  /** Collect parameters from inputs. */
+  const dateRangeSelector = $(config.dateRangeSelector),
+    startDate = dateRangeSelector.data('daterangepicker').startDate;
+    // endDate = dateRangeSelector.data('daterangepicker').endDate,
+    // exclusions = session.excludes || [];
 
-$(document).ready(()=> {
-  // setupProjectInput();
-  setupDateRangeSelector();
-
-  /** Url to query the API. */
-  // const url = (
-  //   `https://wikimedia.org/api/rest_v1/metrics/pageviews/top/${pv.getProject()}/${$('#platform-select').val()}` +
-  //   `/${startDate.format(config.timestampFormat)}/${endDate.format(config.timestampFormat)}`
-  // );
   const url = (
-    `https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/2016/01/01`
+    `https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/${startDate.format('YYYY/MM/DD')}`
   );
 
-  // call resetData() or something
-  session.pageData = [];
-  session.pageNames = [];
+  let dfd = $.Deferred();
 
-  $.ajax({
+  return $.ajax({
     url: url,
     dataType: 'json'
   }).success((data)=> {
@@ -250,9 +268,60 @@ $(document).ready(()=> {
       session.pageNames.push(item.article.replace(/_/g, ' '));
     });
 
-    drawData(session.pageData);
-
-    setupArticleSelector();
-    setArticleSelectorDefaults(config.defaults.excludedPages);
+    return dfd.resolve(session.pageData);
   });
+}
+
+/**
+ * Checks value of project input and validates it against site map
+ * @returns {boolean} whether the currently input project is valid
+ */
+function validateProject() {
+  const project = $(config.projectInput).val();
+  if (siteDomains.includes(project)) {
+    $('.validate').remove();
+    $('.select2-selection--multiple').removeClass('disabled');
+  } else {
+    resetView();
+    writeMessage(
+      `<a href='//${project}'>${project}</a> is not a
+       <a href='//meta.wikipedia.org/w/api.php?action=sitematrix&formatversion=2'>valid project</a>`,
+      'validate', true
+    );
+    $('.select2-selection--multiple').addClass('disabled');
+    return true;
+  }
+}
+
+/**
+ * Writes message just below the chart
+ * @param {string} message - message to write
+ * @param {boolean} clear - whether to clear any existing messages
+ * @returns {jQuery} - jQuery object of message container
+ */
+function writeMessage(message, clear) {
+  if (clear) {
+    pv.clearMessages();
+  }
+  return $('.message-container').append(
+    `<p class='error-message'>${message}</p>`
+  );
+}
+
+$(document).ready(()=> {
+  // setupProjectInput();
+  setupDateRangeSelector();
+  popParams();
+
+  /** Url to query the API. */
+  // const url = (
+  //   `https://wikimedia.org/api/rest_v1/metrics/pageviews/top/${pv.getProject()}/${$('#platform-select').val()}` +
+  //   `/${startDate.format(config.timestampFormat)}/${endDate.format(config.timestampFormat)}`
+  // );
+
+  // call resetData() or something
+  session.pageData = [];
+  session.pageNames = [];
+
+
 });
