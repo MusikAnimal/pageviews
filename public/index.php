a883009a7fed3008fc9873f395915389c6d7e955
Kaldari <rkaldari@wikimedia.org>
2016-02-26 16:01:59 -0600

    adding internationalization support for server-side code
    The JavaScript code still does not have i18n support.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+$I18N = new Intuition( 'pageviews' );
+$I18N->registerDomain( 'pageviews', __DIR__ . '/../messages' );
+?>
 <!-- Pageviews Comparison tool -->
 <!-- Original code forked from https://gist.github.com/marcelrf/49738d14116fd547fe6d courtesy of marcelrf -->
 <!-- Copyright 2016 MusikAnimal -->
@@ -9,9 +14,9 @@
     <meta content='yes' name='apple-mobile-web-app-capable'>
     <meta content='black-translucent' name='apple-mobile-web-app-status-bar-style'>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' name='viewport'>
-    <link href='/pageviews/application.css' rel='stylesheet'>
-    <script src='/pageviews/application.js'></script>
-    <title>Pageviews Analysis</title>
+    <link href='application.css' rel='stylesheet'>
+    <script src='application.js'></script>
+    <title><?= $I18N->msg( 'title' ) ?></title>
   </head>
   <body>
     <div class='container'>
@@ -21,7 +26,7 @@
           <div class='col-lg-10 text-center'>
             <h4>
               <strong>
-                Pageviews Analysis
+                <?= $I18N->msg( 'title' ) ?>
               </strong>
             </h4>
           </div>
@@ -33,47 +38,48 @@
         <div class='row aqs-row options'>
           <!-- Date range selector -->
           <div class='col-lg-4'>
-            <label for='range-input'>Dates</label>
+            <label for='range-input'><?= $I18N->msg( 'dates' ) ?></label>
+            <?php
+              $datelinks = "<a data-value='10' href='#'>10</a>"
+                . " <a data-value='20' href='#'>20</a>"
+                . " <a data-value='30' href='#'>30</a>"
+                . " <a data-value='60' href='#'>60</a>"
+                . " <a data-value='90' href='#'>90</a>";
+            ?>
             <span class='date-latest'>
-              Latest
-              <a data-value='10' href='#'>10</a>
-              <a data-value='20' href='#'>20</a>
-              <a data-value='30' href='#'>30</a>
-              <a data-value='60' href='#'>60</a>
-              <a data-value='90' href='#'>90</a>
-              days
+              <?= $I18N->msg( 'latest-days', array( 'variables' => array( $datelinks ) ) ) ?>
             </span>
             <input class='form-control aqs-date-range-selector' id='range-input'>
           </div>
           <!-- Project selector -->
           <div class='col-lg-2'>
-            <label for='project-input'>Project</label>
+            <label for='project-input'><?= $I18N->msg( 'project' ) ?></label>
             <input class='form-control aqs-project-input' id='project-input' placeholder='en.wikipedia.org'>
           </div>
           <!-- Advanced options -->
           <div class='col-lg-2'>
-            <label for='platform-select'>Platform</label>
+            <label for='platform-select'><?= $I18N->msg( 'platform' ) ?></label>
             <select class='form-control' id='platform-select'>
-              <option value='all-access'>All</option>
-              <option value='desktop'>Desktop</option>
-              <option value='mobile-app'>Mobile app</option>
-              <option value='mobile-web'>Mobile web</option>
+              <option value='all-access'><?= $I18N->msg( 'all' ) ?></option>
+              <option value='desktop'><?= $I18N->msg( 'desktop' ) ?></option>
+              <option value='mobile-app'><?= $I18N->msg( 'mobile-app' ) ?></option>
+              <option value='mobile-web'><?= $I18N->msg( 'mobile-web' ) ?></option>
             </select>
           </div>
           <div class='col-lg-2'>
-            <label for='agent-select'>Agent</label>
+            <label for='agent-select'><?= $I18N->msg( 'agent' ) ?></label>
             <select class='form-control' id='agent-select'>
-              <option value='all-agents'>All</option>
-              <option selected='selected' value='user'>User</option>
-              <option value='spider'>Spider</option>
-              <option value='bot'>Bot</option>
+              <option value='all-agents'><?= $I18N->msg( 'all' ) ?></option>
+              <option selected='selected' value='user'><?= $I18N->msg( 'user' ) ?></option>
+              <option value='spider'><?= $I18N->msg( 'spider' ) ?></option>
+              <option value='bot'><?= $I18N->msg( 'bot' ) ?></option>
             </select>
           </div>
         </div>
         <!-- Article selector -->
         <div class='row aqs-row'>
           <div class='col-lg-10'>
-            <label for='article-input'>Pages</label>
+            <label for='article-input'><?= $I18N->msg( 'pages' ) ?></label>
             <!-- Button trigger modal -->
             <!-- %a.pull-right{href: "#", "data-toggle": "modal", "data-target": "#import-modal"} -->
             <!-- Import -->
@@ -84,9 +90,8 @@
         <div class='chart-container col-lg-10 loading'>
           <!-- inline styles since the ad blocker might block loading of assets -->
           <div class='ad-block-notice text-center'>
-            Chart not appearing? Please ensure you have all ad blockers disabled or have
-            <span style='font-family: Consolas, Lucida Console, monospace;'>/pageviews/*</span>
-            whitelisted.
+          	<?php $url = "<span style='font-family: Consolas, Lucida Console, monospace;'>/pageviews/*</span>"; ?>
+            <?= $I18N->msg( 'chart-error', array( 'variables' => array( $url ) ) ) ?>
           </div>
           <canvas class='aqs-chart'></canvas>
         </div>
@@ -95,14 +100,14 @@
         <div class='col-lg-10' id='chart-legend'></div>
         <!-- Other links -->
         <div class='col-lg-10 data-links'>
-          <a data-target='#chart-type-modal' data-toggle='modal' href='#'>Change chart type</a>
+          <a data-target='#chart-type-modal' data-toggle='modal' href='#'><?= $I18N->msg( 'change-chart' ) ?></a>
           &bullet;
-          <a data-target='#settings-modal' data-toggle='modal' href='#'>Settings</a>
+          <a data-target='#settings-modal' data-toggle='modal' href='#'><?= $I18N->msg( 'settings' ) ?></a>
           &bullet;
-          Download as
-          <a class='download-csv' href='#'>CSV</a>
+          <?php $csvlink = "<a class='download-csv' href='#'>" . $I18N->msg( 'csv' ) . "</a>"; ?>
+          <?= $I18N->msg( 'download', array( 'variables' => array( $csvlink ) ) ) ?>
           &middot;
-          <a class='download-json' href='#'>JSON</a>
+          <a class='download-json' href='#'><?= $I18N->msg( 'json' ) ?></a>
         </div>
         <footer class='col-lg-10'>
           <span>
@@ -112,21 +117,22 @@
             <a href='https://wikimediafoundation.org/wiki/User:Mforns_(WMF)'>Marcel Ruiz Forns</a>.
           </span>
           <span class='nowrap'>
-            Hosted with
-            <span class='heart'>&hearts;</span>
-            on
-            <a href='https://wikitech.wikimedia.org/wiki/Portal:Tool_Labs'>Tool Labs</a>.
+          	<?php
+          		$heart = "<span class='heart'>&hearts;</span>";
+          		$host = "<a href='https://wikitech.wikimedia.org/wiki/Portal:Tool_Labs'>" . $I18N->msg( 'tool-labs' ) . "</a>";
+          	?>
+            <?= $I18N->msg( 'hosted', array( 'variables' => array( $heart, $host ) ) ) ?>
           </span>
           <div>
-            <a href='/pageviews/faq'>FAQ</a>
+            <a href='/pageviews/faq'><?= $I18N->msg( 'faq' ) ?></a>
             &middot;
-            <a data-target='#disclaimer-modal' data-toggle='modal' href='#'>Disclaimer</a>
+            <a data-target='#disclaimer-modal' data-toggle='modal' href='#'><?= $I18N->msg( 'disclaimer' ) ?></a>
             &middot;
-            <a href='/pageviews/url_structure'>URL Structure</a>
+            <a href='/pageviews/url_structure'><?= $I18N->msg( 'url-structure' ) ?></a>
             &middot;
-            <a href='https://github.com/MusikAnimal/pageviews'>View source</a>
+            <a href='https://github.com/MusikAnimal/pageviews'><?= $I18N->msg( 'view-source' ) ?></a>
             &middot;
-            <a href='https://github.com/MusikAnimal/pageviews/issues'>Report an issue</a>
+            <a href='https://github.com/MusikAnimal/pageviews/issues'><?= $I18N->msg( 'report-issue' ) ?></a>
           </div>
         </footer>
         <div class='modal fade' id='disclaimer-modal' role='dialog' tabindex='-1'>
@@ -136,21 +142,20 @@
                 <button arialabel='Close' class='close' data-dismiss='modal' type='button'>
                   <span ariahidden='true'>&times;</span>
                 </button>
-                <h4 class='modal-title'>Disclaimer</h4>
+                <h4 class='modal-title'><?= $I18N->msg( 'disclaimer' ) ?></h4>
               </div>
               <div class='modal-body'>
                 <div>
-                  Pageviews Analysis fetches data via the <a href="https://wikimedia.org/api/rest_v1/?doc#/">Wikimedia REST API</a>.
-                  This API is still listed to be in an
-                  <b><a href="https://www.mediawiki.org/wiki/API_versioning#Experimental">experimental phase</a>.</b>
-                  This means functionality <i>could</i> <b>break at any time without warning</b>. The
-                  <a href="https://en.wikipedia.org/wiki/User:MusikAnimal">maintainer</a> of Pageviews Anaylsis will work to stay
-                  up-to-date with API changes, but no promises can be made that the tool will indefinitely be stable until the API
-                  is released as stable.
+                	<?php
+										$api = "<a href='https://wikimedia.org/api/rest_v1/?doc#/'>" . $I18N->msg( 'rest-api' ) . "</a>";
+										$experimental = "<b><a href='https://www.mediawiki.org/wiki/API_versioning#Experimental'>" . $I18N->msg( 'experimental' ) . "</a></b>";
+										$maintainer = "<a href='https://en.wikipedia.org/wiki/User:MusikAnimal'>" . $I18N->msg( 'maintainer' ) . "</a>";
+									?>
+                  <?= $I18N->msg( 'disclaimer-text', array( 'variables' => array( $api, $experimental, $maintainer ) ) ) ?>
                 </div>
               </div>
               <div class='modal-footer'>
-                <button class='btn btn-primary' data-dismiss='modal' type='button'>OK</button>
+                <button class='btn btn-primary' data-dismiss='modal' type='button'><?= $I18N->msg( 'ok' ) ?></button>
               </div>
             </div>
           </div>
@@ -162,55 +167,55 @@
                 <button arialabel='Close' class='close' data-dismiss='modal' type='button'>
                   <span ariahidden='true'>&times;</span>
                 </button>
-                <h4 class='modal-title'>Chart types</h4>
+                <h4 class='modal-title'><?= $I18N->msg( 'chart-types' ) ?></h4>
               </div>
               <div class='modal-body'>
                 <div>
-                  <strong>Articles by daily pageviews</strong>
+                  <strong><?= $I18N->msg( 'daily-pageviews' ) ?></strong>
                   <span class='modal-chart-type'>
                     <a data-dismiss='modal' data-type='Line' href='#'>
                       <img src='images/line-chart-small.jpg'>
-                      Line
+                      <?= $I18N->msg( 'line' ) ?>
                     </a>
                   </span>
                   <span class='modal-chart-type'>
                     <a data-dismiss='modal' data-type='Bar' href='#'>
                       <img src='images/bar-chart-small.jpg'>
-                      Bar
+                      <?= $I18N->msg( 'bar' ) ?>
                     </a>
                   </span>
                   <span class='modal-chart-type'>
                     <a data-dismiss='modal' data-type='Radar' href='#'>
                       <img src='images/radar-chart-small.jpg'>
-                      Radar
+                      <?= $I18N->msg( 'radar' ) ?>
                     </a>
                   </span>
                 </div>
                 <hr>
                 <div>
-                  <strong>Articles by total number of pageviews</strong>
+                  <strong><?= $I18N->msg( 'total-pageviews' ) ?></strong>
                   <span class='modal-chart-type'>
                     <a data-dismiss='modal' data-type='Pie' href='#'>
                       <img src='images/pie-chart-small.jpg'>
-                      Pie
+                      <?= $I18N->msg( 'pie' ) ?>
                     </a>
                   </span>
                   <span class='modal-chart-type'>
                     <a data-dismiss='modal' data-type='Doughnut' href='#'>
                       <img src='images/doughnut-chart-small.jpg'>
-                      Doughnut
+                      <?= $I18N->msg( 'doughnut' ) ?>
                     </a>
                   </span>
                   <span class='modal-chart-type'>
                     <a data-dismiss='modal' data-type='PolarArea' href='#'>
                       <img src='images/polararea-chart-small.jpg'>
-                      Polar Area
+                      <?= $I18N->msg( 'polar-area' ) ?>
                     </a>
                   </span>
                 </div>
               </div>
               <div class='modal-footer'>
-                <button class='btn btn-default' data-dismiss='modal' type='button'>Cancel</button>
+                <button class='btn btn-default' data-dismiss='modal' type='button'><?= $I18N->msg( 'cancel' ) ?></button>
               </div>
             </div>
           </div>
@@ -222,48 +227,50 @@
                 <button arialabel='Close' class='close' data-dismiss='modal' type='button'>
                   <span ariahidden='true'>&times;</span>
                 </button>
-                <h4 class='modal-title'>Settings</h4>
+                <h4 class='modal-title'><?= $I18N->msg( 'settings' ) ?></h4>
               </div>
               <form class='modal-body'>
                 <p>
-                  Your settings will be remembered on the same browser and computer.
+                  <?= $I18N->msg( 'settings-notice' ) ?>
                 </p>
                 <fieldset class='radio-list'>
-                  Search method:
+                  <?= $I18N->msg( 'search-method' ) ?>
                   <div class='radio'>
                     <label>
                       <input name='autocomplete' type='radio' value='autocomplete'>
-                      Autocompletion (<a href="https://www.mediawiki.org/wiki/API:Prefixsearch" target="_blank">prefixsearch</a>)
+                      <?php $prefixsearchlink = "<a href='https://www.mediawiki.org/wiki/API:Prefixsearch' target='_blank'>" . $I18N->msg( 'prefixsearch' ) . "</a>"; ?>
+                      <?= $I18N->msg( 'autocompletion', array( 'variables' => array( $prefixsearchlink ) ) ) ?>
                     </label>
                   </div>
                   <div class='radio'>
                     <label>
                       <input name='autocomplete' type='radio' value='autocomplete_redirects'>
-                      Autocompletion including redirects (<a href="https://www.mediawiki.org/wiki/API:Opensearch" target="_blank">opensearch</a>)
+                      <?php $opensearchlink = "<a href='https://www.mediawiki.org/wiki/API:Opensearch' target='_blank'>" . $I18N->msg( 'opensearch' ) . "</a>"; ?>
+                      <?= $I18N->msg( 'autocompletion-redirects', array( 'variables' => array( $opensearchlink ) ) ) ?>
                     </label>
                   </div>
                   <div class='radio'>
                     <label>
                       <input name='autocomplete' type='radio' value='no_autocomplete'>
-                      No autocompletion
+                      <?= $I18N->msg( 'no-autocompletion' ) ?>
                     </label>
                   </div>
                 </fieldset>
                 <fieldset class='radio-list palette-list'>
-                  Color scheme:
+                  <?= $I18N->msg( 'color-scheme' ) ?>
                 </fieldset>
                 <fieldset>
-                  Other options:
+                  <?= $I18N->msg( 'other-options' ) ?>
                   <div class='checkbox'>
                     <label>
                       <input name='numericalFormatting' type='checkbox' value>
-                      Format numerical data
+                      <?= $I18N->msg( 'format-numbers' ) ?>
                     </label>
                   </div>
                   <div class='checkbox'>
                     <label>
                       <input name='localizeDateFormat' type='checkbox' value>
-                      Localize date format
+                      <?= $I18N->msg( 'localize-dates' ) ?>
                     </label>
                   </div>
                   <!-- %div.checkbox -->
@@ -273,8 +280,8 @@
                 </fieldset>
               </form>
               <div class='modal-footer'>
-                <button class='btn btn-default cancel-settings-btn' data-dismiss='modal' type='button'>Cancel</button>
-                <button class='btn btn-primary save-settings-btn' data-dismiss='modal' type='button'>Save</button>
+                <button class='btn btn-default cancel-settings-btn' data-dismiss='modal' type='button'><?= $I18N->msg( 'cancel' ) ?></button>
+                <button class='btn btn-primary save-settings-btn' data-dismiss='modal' type='button'><?= $I18N->msg( 'save' ) ?></button>
               </div>
             </div>
           </div>
