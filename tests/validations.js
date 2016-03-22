90dc57cfef28b7363877fb3c1aa0c6a92f9e0979
MusikAnimal <ziemba0123@gmail.com>
2016-03-22 00:09:39 -0400

    acceptance tests for various user input

--- tests/validations.js
+++ tests/validations.js
@@ -1,10 +1,13 @@
 module.exports = {
-  'Invalid project shows error and disables form': browser => {
-    browser
+  'Invalid project shows error and disables form': client => {
+    client
       .url('http://localhost/pageviews/index')
-      .waitForElementVisible('body', 1000)
-      .setValue('.aqs-project-input', ['en.nonexistent.org', browser.Keys.TAB])
-      .pause(1000)
+      .waitForElementVisible('body', 1000);
+
+    // apparently the only way to clear the value (clearValue does not work in FF)
+    client.setValue('.aqs-project-input', [client.Keys.CONTROL, client.Keys.SHIFT, 'a', client.Keys.BACKSPACE])
+      .setValue('.aqs-project-input', ['en.nonexistent.org', client.Keys.TAB])
+      .pause(500)
       .assert.containsText('.error-message', 'en.nonexistent.org')
       .assert.cssClassPresent('.select2-selection--multiple', 'disabled')
       .assert.elementNotPresent('canvas')
