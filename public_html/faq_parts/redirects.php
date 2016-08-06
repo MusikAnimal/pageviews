dc0f9d8b82aa25a28ced690e628e497c40346047
MusikAnimal <ziemba0123@gmail.com>
2016-08-06 13:42:52 -0700

    Add FAQ entry explaining how pageviews of redirects are counted

---
+++ public_html/faq_parts/redirects.php
@@ -0,0 +1,9 @@
+<li class="list-group-item" id="redirects">
+  <p>
+    <strong><?php echo $I18N->msg( 'faq-redirects-title' ); ?></strong>
+  </p>
+  <p>
+    <?php $redirectViewsLink = "<a href='/redirectviews'>" . $I18N->msg( 'redirectviews' ) . "</a>"; ?>
+    <?php echo $I18N->msg( 'faq-redirects-body', array( 'variables' => array( $redirectViewsLink ) ) ); ?>
+  </p>
+</li>
