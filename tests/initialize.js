d4d63758543eabb8c50dcff2b667e84cf7d21571
MusikAnimal <ziemba0123@gmail.com>
2016-12-28 14:00:09 -0500

    maybe now?

--- tests/initialize.js
+++ tests/initialize.js
@@ -67,7 +67,7 @@ module.exports = {
         ['Michael Phelps']
       )).to.equal(true);
     });
-    client.expect.element('#topview-entry-2').text.to.contain('Suicide Squad (film)').after(10000);
+    client.expect.element('#topview-entry-2').text.to.contain('Suicide Squad (film)').after(15000);
     client.end();
   }
 };
