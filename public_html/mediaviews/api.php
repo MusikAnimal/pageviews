7f51c2792ac4576d775dedd50d09a5373ac1179e
Nintendofan885 <66736551+Nintendofan885@users.noreply.github.com>
2020-07-28 16:34:40 +0100

    Update Toolforge URL

--- public_html/mediaviews/api.php
+++ public_html/mediaviews/api.php
@@ -4,6 +4,6 @@
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Accept: application/json']);
 curl_setopt($ch, CURLOPT_URL, $_GET['endpoint']);
-curl_setopt($ch, CURLOPT_USERAGENT, 'Mediaviews Analysis; https://tools.wmflabs.org/mediaviews / https://en.wikipedia.org/wiki/User_talk:MusikAnimal');
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mediaviews Analysis; https://mediaviews.toolforge.org / https://en.wikipedia.org/wiki/User_talk:MusikAnimal');
 $result = curl_exec($ch);
 curl_close($ch);
